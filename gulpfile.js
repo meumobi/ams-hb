db78213b5132958366792736082aba3b9f5f77ef
victor dias <victor.dias@meumobi.com>
2018-02-14 14:11:20 +0000

    TOOLS: #18, add release task to strip debug logs

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,13 @@
 
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var stripDebug  = require('gulp-strip-debug');
+ 
+gulp.task('release', () =>
+    gulp.src('js/hb_ams-lib.js')
+        .pipe(stripDebug())
+        .pipe(gulp.dest('dist'))
+);
 
 // Static server
 gulp.task('browser-sync', function() {
