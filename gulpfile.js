1ebfc2b45401e138f3ca5874c0c8aa5d39a67432
Daniel Conte <daniel.conte@meumobi.com>
2018-02-19 17:37:17 -0300

    ENHANCE: Closes #18, gulp release

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,19 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var stripDebug  = require('gulp-strip-debug');
+var minify = require('gulp-minify');
  
 gulp.task('release', () =>
     gulp.src('js/hb_ams-lib.js')
         .pipe(stripDebug())
+        .pipe(minify({
+            ext:{
+                src:'-debug.js',
+                min:'.js'
+            }
+        }))
         .pipe(gulp.dest('dist'))
 );
-
 // Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
