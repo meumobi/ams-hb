feb51c1d933bea3956349ddaf1f158f90740acd2
victor dias <victor.dias@meumobi.com>
2018-04-23 16:54:24 +0100

    remove useless files and move public directory to www

--- www/js/hb_mas.js
+++ www/js/hb_mas.js
@@ -10,20 +10,22 @@ var hbAMS = {
     }
 };
 
+var adServer = {};
+
+adServer.config = {
+    protocol: 'https',
+    server: 'secserv.adtech.de',
+    network: '1502.1',
+    siteid: '670202',
+    params: {
+        loc: '100'
+    }
+};
+
 hbAMS.settings.adUnits = [
-    {
-        code: 6494071,
-        bounds: [
-            { id: 6494072, min: 0, max: 768 },
-            { id: 6494071, min: 769, max: 9999 },
-        ],
-        sizeid: '16',
-        bids: []
-    },
     {
         code: 6489219,
         fif: { usefif: true },
-        //sizes: [[728, 90], [970, 90], [970, 250]],
         sizeMapping: [
             {
                 minWidth: 769,
@@ -72,49 +74,6 @@ hbAMS.settings.adUnits = [
         ]
     },
     {
-        code: 6493810,
-        //sizes: [[320, 50], [320, 100]],
-        sizeMapping: [
-
-            {
-                minWidth: 769,
-                sizes: []
-            },
-            {
-                minWidth: 0,
-                sizes: [[320, 50], [320, 100], [320, 53], [320, 160], [320, 320]]
-            },
-        ],
-        bids: [
-            {
-                bidder: 'smartadserver',
-                params: {
-                    domain: '//prg.smartadserver.com',
-                    siteId: '170999',
-                    pageId: '845267',
-                    formatId: '46454'
-                }
-            },
-            {
-                bidder: 'widespace',
-                params: {
-                    sid: '620f5133-4b47-4bbf-823c-6b4fe293b6e1',
-                    currency: 'USD'
-                }
-            },
-            {
-                bidder: 'rubicon',
-                params: {
-                    accountId: '14794',
-                    siteId: '83768',
-                    zoneId: '395364',
-                    sizes: '[43]'
-                }
-            },
-
-        ]
-    },
-    {
         code: 6490489,
         fif: { usefif: true },
         autoRefresh: true,
@@ -153,47 +112,6 @@ hbAMS.settings.adUnits = [
                     accountId: '14794',
                     siteId: '83734',
                     zoneId: '395240'
-                }
-            }
-        ]
-    },
-
-    {
-        code: 6494025,
-        fif: { usefif: true },
-        //sizes: [[300, 600], [300, 250]],
-        sizeMapping: [
-            {
-                minWidth: 769,
-                sizes: []
-            },
-            {
-                minWidth: 0,
-                sizes: [[300, 600], [300, 250]]
-            },
-        ],
-        bounds: [
-            { id: 6494025, min: 0, max: 768 },
-            { id: 6490489, min: 769, max: 9999 },
-        ],
-        sizeid: '170',
-        bids: [
-            {
-                bidder: 'smartadserver',
-                params: {
-                    domain: 'https://www8.smartadserver.com',
-                    siteId: '170999',
-                    pageId: '842325',
-                    formatId: '45838'
-                }
-            },
-            {
-                bidder: 'rubicon',
-                params: {
-                    accountId: '14794',
-                    siteId: '83768',
-                    zoneId: '395364',
-                    sizes: [15]
                 }
             }
         ]
