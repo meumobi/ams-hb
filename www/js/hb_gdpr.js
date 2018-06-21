6d40ac34ef66874b21f23fa66fc00326a2af53f2
victor dias <victor.dias@meumobi.com>
2018-06-21 17:00:08 +0100

    refactor: merge gdpr and cmp demo, and version DAC.js

--- www/js/hb_gdpr.js
+++ www/js/hb_gdpr.js
@@ -48,15 +48,48 @@ var adUnits = [{
         siteId: '83734',
         zoneId: '395240'
       }
+    }, 
+    {
+        bidder: "criteo",
+        params: {
+            zoneId: '1119285'
+        }
+    },
+    {
+        bidder: "improvedigital",
+        params: {
+            placementId: '1167201',
+        }
+    },
+    {
+        bidder: 'widespace',
+        params: {
+            sid: '620f5133-4b47-4bbf-823c-6b4fe293b6e1',
+            currency: 'USD'
+        }
     },
   ]
 }];
 
 ADTECH.config.placements["6489219"] = adUnits[0];
-ADTECH.debugMode = true;
+ADTECH.debugMode = false;
 
 var pbams = pbams || {};
 pbams.que = pbams.que || [];
+
+pbams.setConfig({ 
+  priceGranularity: 'dense',
+  cookieSyncDelay: 200,
+});
+if (ADTECH.config.page.gdpr) {
+  pbams.setConfig({ 
+      consentManagement: {
+          cmpApi: 'iab',
+          timeout: 8000,
+          allowAuctionWithoutConsent: true
+      }
+  });
+}
 
 pbams.que.push(function() {
   pbams.addAdUnits(adUnits);
@@ -71,10 +104,11 @@ function initAdserver() {
   ADTECH.executeQueue();  
   console.log("initAdserver - End");
 }
-
+/**
 setTimeout(function() {
   window.alert("Call to Adserver outside prebid callback");
   ADTECH.enqueueAd("6489219");
   ADTECH.executeQueue();  
 }, 9000);
+ */
 
